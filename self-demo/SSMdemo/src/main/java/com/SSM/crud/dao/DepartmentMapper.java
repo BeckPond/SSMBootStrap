af248e349a4f9203cb22207fbc2c7e21073f9d3d
BeckPond <jeremylu0724@gmail.com>
2019-09-11 00:30:23 +0800

    SSM+BootStrap，功能是CRUD+分页查询 Mybatis逆向工程解决工作中可能出现的表过多的问题 Bootstrap优化页面

---
+++ self-demo/SSMdemo/src/main/java/com/SSM/crud/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package com.SSM.crud.dao;
+
+import com.SSM.crud.bean.Department;
+import com.SSM.crud.bean.DepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DepartmentMapper {
+    long countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer deptId);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer deptId);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
